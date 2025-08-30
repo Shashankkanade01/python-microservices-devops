795da03c5b3207f0fbfcf569a0045f686f3a63c4
Shashankkanade01 <shashankkanade01@gmail.com>
2025-08-30 11:28:28 +0530

    Docker Compose (Local Orchestration)

--- frontend/app.py
+++ frontend/app.py
@@ -16,4 +16,4 @@ def index():
     return render_template("index.html", data=data)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
